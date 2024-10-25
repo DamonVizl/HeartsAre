2303dace84e8f5fa97565229172b6aa789a2f5b7
DamonVizl <damon.vizl@gmail.com>
2024-10-25 13:50:52 +1000

    0.00.014
    Added some art and linked up some code

--- Assets/Cards/Card.cs
+++ Assets/Cards/Card.cs
@@ -46,7 +46,7 @@ public class Card
         Debug.Log(suit.ToString() + " vs "+ Resources.Load<Sprite>(suit.ToString()));
         _suitSprite = Resources.Load<Sprite>(suit.ToString()); //suit sprites should be saved in the Resources folder named "heart.png" for e.g.
         _valueSprite = Resources.Load<Sprite>(value.ToString()); //value sprites (1,2,K,4, etc) should be saved in the Resources folder named "king.png" for e.g.
-        _centreImageSprite = Resources.Load<Sprite>("Big"+suit.ToString()); //card centre images should be saved in the Resources folder named "heart_king.png" for e.g. 
+        _centreImageSprite = Resources.Load<Sprite>(value.ToString() + "_" + suit.ToString()); //card centre images should be saved in the Resources folder named "heart_king.png" for e.g. 
         // Set sprite colours based off suit. 
         SetSpriteColours(suit); 
     }
